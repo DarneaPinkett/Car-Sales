dcf7e52269ddc3e5b4f9ba39f9b477475804b0a2
DarneaPinkett <56055954+DarneaPinkett@users.noreply.github.com>
2020-02-29 13:43:59 -0500

    updatedreducer

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,17 +1,39 @@
-export const ADD_ITEM = 'ADD_ITEM';
+import {ADD, REMOVE, UPDATE_TOTAL} from '../actions/index';
+  
+  const initialState = {
+    additionalPrice: 0,
+    car: {
+      price: 26395,
+      name: '2019 Ford Mustang',
+      image:
+        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+      features: []
+    },
+    additionalFeatures: [
+      { id: 1, name: 'V-6 engine', price: 1500 },
+      { id: 2, name: 'Racing detail package', price: 1500 },
+      { id: 3, name: 'Premium sound system', price: 500 },
+      { id: 4, name: 'Rear spoiler', price: 250 }
+    ]
+  };
 
-export function addItem(item) {
-    return {
-        type: ADD_ITEM,
-        payload: item
-    }
-}
-
-export const REMOVE_ITEM = 'REMOVE_ITEM';
-
-export function removeItem(item) {
-    return {
-        type: REMOVE_ITEM,
-        payload: item
+export const carReducer = ( state = initialState, action) => {
+    switch (action.type){
+        case ADD:
+            return {...state, car:{...state.car,
+            features: state.car.features.includes(action.payload) ? [...state.car.features] : [...state.car.features, action.payload]
+            }
+        }
+        case REMOVE:
+            return {...state, car: {...state.car,
+            features: state.car.features.filter(car => car.id !== action.payload.id)
+            }
+        }
+        case UPDATE_TOTAL:
+            return {...state,
+                additionalPrice: state.additionalPrice + action.payload
+            };
+            default:
+                return state;
     }
 }
